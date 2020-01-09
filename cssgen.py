9e5976b8e17557a33b8a9563def92d8ca19f2e88
Moody Rahman <mrahman01@stuy.edu>
2020-01-08 23:25:51 -0500

    init files

---
+++ cssgen.py
@@ -0,0 +1,8 @@
+out = ""
+copy = """.fade{n}{{\nopacity: 0.0;\nanimation: example {n}s 1;\nanimation-fill-mode: forwards;\n}}\n"""
+
+for x in range(6, 100):
+    out = out + "\n\n" + copy.format(n=str(x))
+
+
+print(out)
